8d546cb546b9d2d9d5975f4944321252e610a831
Rami B <ramibasheer9@gmail.com>
2022-08-10 16:02:49 +0200

    apply solution

--- index.js
+++ index.js
@@ -2,8 +2,25 @@
 
 //write your code here
 
+const favRecipe = {
+title: "Lasagna",
+serves: 3,
+ingredients: ["pasta", "meat", "tomatos", "cheese", "milk"]
+
+} 
 
 
+
+console.log(favRecipe.title);
+console.log(`serves: ${favRecipe.serves}`)
+
+for(let i = 0; i < favRecipe.ingredients.length; i++){
+    console.log(favRecipe.ingredients[i]);
+  }
+
+
+
+console.log("--------------------------");
 
 //Create an array of objects, where each object describes a book and has properties for the title (a string), author (a string), and alreadyRead (a boolean indicating if you read it yet).
 //Iterate through the array of books. For each book, log the book title and book author like so: "The Hobbit by J.R.R. Tolkien".
@@ -12,3 +29,74 @@
 
 
 //write your code here
+
+const books = [
+    {
+        title: "into the wild",
+        author: "Jon Krakauer",
+        alreadyRead: true,
+        
+    },
+    {
+        title: "will",
+        author: "Will Smith",
+        alreadyRead: false,
+    },
+    {
+        title: "The Subtle Art of not giving a F",
+        author: "Mark Manson",
+        alreadyRead: true,
+    }
+]
+
+
+for (i = 2; i < books.length; i++) {
+    console.log(`"${books[0].title} by ${books[0].author}". `);
+}
+
+for (i = 2; i < books.length; i++) {
+    console.log(`"${books[1].title} by ${books[1].author}". `);
+}
+
+for (i = 2; i < books.length; i++) {
+    console.log(`"${books[2].title} by ${books[2].author}". `);
+}
+
+console.log("-----------------------");
+
+
+if (books[0].alreadyRead === true) {
+    console.log(`You already read "${books[0].title} by ${books[0].alreadyRead}"`);
+}else {
+    console.log(`You still need to read ${books[0].title} by ${books[0].alreadyRead}"`);
+};
+
+
+if (books[1].alreadyRead === true) {
+    console.log(`You already read "${books[1].title} by ${books[1].alreadyRead}"`);
+}else {
+    console.log(`You still need to read ${books[1].title} by ${books[1].alreadyRead}"`);
+};
+
+
+if (books[2].alreadyRead === true) {
+    console.log(`You already read "${books[2].title} by ${books[2].alreadyRead}"`);
+}else {
+    console.log(`You still need to read ${books[2].title} by ${books[2].alreadyRead}"`);
+};
+
+
+
+
+
+
+
+
+console.log("----------------------------");
+
+
+
+
+
+
+
